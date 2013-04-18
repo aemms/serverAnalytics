d309ccd10a7c54f0e672787e271d7bcca66283fb
Saagar <saagar.hemrajani@gmail.com>
2013-04-18 12:23:29 +0200

    Finished Printer Queue

--- src/com/comp1008/serveranalytics/ui/PrinterActivity.java
+++ src/com/comp1008/serveranalytics/ui/PrinterActivity.java
@@ -27,6 +27,7 @@ public class PrinterActivity extends Activity {
 	private Printer printer;
 	private DataController data;
 	private QueueItem qitem;
+	private int count;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,85 +53,160 @@ public class PrinterActivity extends Activity {
     	TextView printerTonerView = (TextView)findViewById(R.id.tonerremaining_content);
     	printerTonerView.setText(printer.getTonerRemaining());
     	
-    	//Fills the 3 rows of the table with the printer queue data
-    	Iterator<QueueItem> it = printer.getQueueItemsIterator();
-    	while (it.hasNext()) {
-    		qitem = it.next();	
-    		TextView qnum1view = (TextView)findViewById(R.id.qnum1);
-        	qnum1view.setText(qitem.getQnum());
-        	
-        	TextView name1view = (TextView)findViewById(R.id.name1);
-        	name1view.setText(qitem.getFileName());
-        	
-        	TextView status1view = (TextView)findViewById(R.id.status1);
-        	status1view.setText(qitem.getStatus());
-        	
-        	TextView user1view = (TextView)findViewById(R.id.user1);
-        	user1view.setText(qitem.getStatus());
-        	
-        	TextView pages1view = (TextView)findViewById(R.id.pages1);
-        	pages1view.setText(qitem.getPages());
-        	
-        	TextView size1view = (TextView)findViewById(R.id.size1);
-        	size1view.setText(qitem.getSize());
-        	
-        	TextView time1view = (TextView)findViewById(R.id.time1);
-        	time1view.setText(qitem.getTime());
-        	
-        	qitem = it.next();	
-        	TextView qnum2view = (TextView)findViewById(R.id.qnum2);
-        	qnum2view.setText(qitem.getQnum());
-        	
-        	TextView name2view = (TextView)findViewById(R.id.name2);
-        	name2view.setText(qitem.getFileName());
-        	
-        	TextView status2view = (TextView)findViewById(R.id.status2);
-        	status2view.setText(qitem.getStatus());
-        	
-        	TextView user2view = (TextView)findViewById(R.id.user2);
-        	user2view.setText(qitem.getStatus());
-        	
-        	TextView pages2view = (TextView)findViewById(R.id.pages2);
-        	pages2view.setText(qitem.getPages());
-        	
-        	TextView size2view = (TextView)findViewById(R.id.size2);
-        	size2view.setText(qitem.getSize());
-        	
-        	TextView time2view = (TextView)findViewById(R.id.time2);
-        	time2view.setText(qitem.getTime());
-        	
-        	qitem = it.next();	
-        	TextView qnum3view = (TextView)findViewById(R.id.qnum3);
-        	qnum3view.setText(qitem.getQnum());
-        	
-        	TextView name3view = (TextView)findViewById(R.id.name3);
-        	name3view.setText(qitem.getFileName());
-        	
-        	TextView status3view = (TextView)findViewById(R.id.status3);
-        	status3view.setText(qitem.getStatus());
-        	
-        	TextView user3view = (TextView)findViewById(R.id.user3);
-        	user3view.setText(qitem.getStatus());
-        	
-        	TextView pages3view = (TextView)findViewById(R.id.pages3);
-        	pages3view.setText(qitem.getPages());
-        	
-        	TextView size3view = (TextView)findViewById(R.id.size3);
-        	size3view.setText(qitem.getSize());
-        	
-        	TextView time3view = (TextView)findViewById(R.id.time3);
-        	time3view.setText(qitem.getTime());
-        	
-    	  }
-    
+    	//Fills a table with the printer queue data
+    	count = 0;
+    	//Creates table layout
+    	TableLayout qtable = (TableLayout) findViewById(R.id.queueTable);
     	
-    	/*uses iterator to Display 3 row table of printer queue
-    	 * TO BE DONE: 
-    	 * - Make the table dynamic
-    	 * - Make it fit the screen
-    	 * - Make it organize itself by queue
-    	 * 
-    	*/
+    	//Create row of headings
+    	TableRow header = new TableRow(this);
+    	header.setId(10);
+    	header.setBackgroundColor(Color.DKGRAY);
+    	header.setLayoutParams(new LayoutParams(
+    	LayoutParams.FILL_PARENT,
+    	LayoutParams.WRAP_CONTENT));
+    	
+    	TextView headerQueue = new TextView(this);
+    	headerQueue.setId(20); 
+    	headerQueue.setText("Queue");
+    	headerQueue.setPadding(7, 7, 7, 7);
+    	headerQueue.setTextColor(Color.WHITE);
+		header.addView(headerQueue);
+    	
+		TextView headerName = new TextView(this);
+		headerName.setId(20); 
+		headerName.setText("Name");
+		headerName.setPadding(7, 7, 7, 7);
+		headerName.setTextColor(Color.WHITE);
+		header.addView(headerName);
+		
+		TextView headerStatus = new TextView(this);
+    	headerStatus.setId(20); 
+    	headerStatus.setText("Status");
+    	headerStatus.setPadding(7, 7, 7, 7);
+    	headerStatus.setTextColor(Color.WHITE);
+		header.addView(headerStatus);
+		
+		TextView headerUser = new TextView(this);
+    	headerUser.setId(20); 
+    	headerUser.setText("User");
+    	headerUser.setPadding(7, 7, 7, 7);
+    	headerUser.setTextColor(Color.WHITE);
+		header.addView(headerUser);
+		
+		TextView headerPages = new TextView(this);
+    	headerPages.setId(20); 
+    	headerPages.setText("Pages");
+    	headerPages.setPadding(7, 7, 7, 7);
+    	headerPages.setTextColor(Color.WHITE);
+		header.addView(headerPages);
+		
+		TextView headerSize = new TextView(this);
+		headerSize.setId(20); 
+		headerSize.setText("Size");
+		headerSize.setPadding(7, 7, 7, 7);
+		headerSize.setTextColor(Color.WHITE);
+		header.addView(headerSize);
+		
+		TextView headerTime = new TextView(this);
+		headerTime.setId(20); 
+		headerTime.setText("Time");
+		headerTime.setPadding(7, 7, 7, 7);
+		headerTime.setTextColor(Color.WHITE);
+		header.addView(headerTime);
+		
+		//Populates row of headings
+		qtable.addView(header, new TableLayout.LayoutParams(
+                LayoutParams.FILL_PARENT,
+                LayoutParams.WRAP_CONTENT));
+		
+		//Iterator goes through queueItems
+    	Iterator<QueueItem> itr = printer.getQueueItemsIterator();
+    	while (itr.hasNext()) {
+    		qitem = itr.next();
+    		String queue = qitem.getQnum();
+    		String name = qitem.getFileName();
+    		String status = qitem.getStatus();
+    		String user = qitem.getUser();
+    		String pages = qitem.getPages();
+    		String size = qitem.getSize();
+    		String time = qitem.getTime();
+    		
+    		//Creates new row to store item in queue
+    		TableRow tr = new TableRow(this); 
+    		if(count%2!=0) tr.setBackgroundColor(Color.GRAY);
+    		tr.setId(100+count);
+    		tr.setLayoutParams(new LayoutParams(
+    				LayoutParams.FILL_PARENT,
+    				LayoutParams.WRAP_CONTENT));
+    		
+    		
+    		//Creates Textviews and stores individual information about print job
+    		TextView queueTextView = new TextView(this);
+    		queueTextView.setId(200+count); 
+    		queueTextView.setText(queue);
+    		queueTextView.setPadding(2, 0, 5, 0);
+    		if (count%2!=0) queueTextView.setTextColor(Color.WHITE);
+    		if (count%2 ==0) queueTextView.setTextColor(Color.BLACK);
+    		tr.addView(queueTextView);
+    		
+    		TextView nameTextView = new TextView(this);
+    		nameTextView.setId(200+count); 
+    		nameTextView.setText(name);
+    		nameTextView.setPadding(2, 0, 5, 0);
+    		if (count%2!=0) nameTextView.setTextColor(Color.WHITE);
+    		if (count%2 ==0) nameTextView.setTextColor(Color.BLACK);
+    		tr.addView(nameTextView);
+    	
+    		TextView statusTextView = new TextView(this);
+    		statusTextView.setId(200+count); 
+    		statusTextView.setText(status);
+    		statusTextView.setPadding(2, 0, 5, 0);
+    		if (count%2!=0) statusTextView.setTextColor(Color.WHITE);
+    		if (count%2 ==0) statusTextView.setTextColor(Color.BLACK);
+    		tr.addView(statusTextView);
+    	
+    		TextView userTextView = new TextView(this);
+    		userTextView.setId(200+count); 
+    		userTextView.setText(user);
+    		userTextView.setPadding(2, 0, 5, 0);
+    		if (count%2!=0) userTextView.setTextColor(Color.WHITE);
+    		if (count%2 ==0) userTextView.setTextColor(Color.BLACK);
+    		tr.addView(userTextView);
+    		
+    		TextView pagesTextView = new TextView(this);
+    		pagesTextView.setId(200+count); 
+    		pagesTextView.setText(pages);
+    		pagesTextView.setPadding(2, 0, 5, 0);
+    		if (count%2!=0) pagesTextView.setTextColor(Color.WHITE);
+    		if (count%2 ==0) pagesTextView.setTextColor(Color.BLACK);
+    		tr.addView(pagesTextView);
+    		
+    		TextView sizeTextView = new TextView(this);
+    		sizeTextView.setId(200+count); 
+    		sizeTextView.setText(size);
+    		sizeTextView.setPadding(2, 0, 5, 0);
+    		if (count%2!=0) sizeTextView.setTextColor(Color.WHITE);
+    		if (count%2 ==0) sizeTextView.setTextColor(Color.BLACK);
+    		tr.addView(sizeTextView);
+    	
+    		
+    		TextView timeTextView = new TextView(this);
+    		timeTextView.setId(200+count); 
+    		timeTextView.setText(time);
+    		timeTextView.setPadding(2, 0, 5, 0);
+    		if (count%2!=0) timeTextView.setTextColor(Color.WHITE);
+    		if (count%2 ==0) timeTextView.setTextColor(Color.BLACK);
+    		tr.addView(timeTextView);
+    	
+    		//Appends row to the Queue Table and increments counter
+    		qtable.addView(tr, new TableLayout.LayoutParams(
+                    LayoutParams.FILL_PARENT,
+                    LayoutParams.WRAP_CONTENT));
+	       count++;
+	    
+    	}
+    
 	}
 	
 	
